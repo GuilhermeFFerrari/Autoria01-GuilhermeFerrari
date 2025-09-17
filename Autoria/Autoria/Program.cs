15ea97ab01fe32dcaf51ed58ef08d18314dfac57
GuilhermeFerrari <ferrarifguilherme@gmail.com>
2025-09-17 12:13:50 -0300

    atv finalizada autoria

---
+++ Autoria/Autoria/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+    {
+        Random random = new Random();
+        int num, soma;
+        string escolhaUsuario, respostaUsuario;
+
+        Console.WriteLine("Jogo do Par ou Ímpar");
+        Console.WriteLine("Você quer jogar? (s/n)");
+        respostaUsuario = Console.ReadLine().ToUpper();
+        if (respostaUsuario == "S")
+        {
+            Console.Write("Digite par ou impar: ");
+            escolhaUsuario = Console.ReadLine().ToUpper();
+            Console.Write("Digite um número entre 0 e 10: ");
+            int numeroUsuario = int.Parse(Console.ReadLine());
+
+            num = random.Next(0, 11);
+            soma = numeroUsuario + num;
+
+            Console.WriteLine($"Você escolheu {numeroUsuario} e o computador escolheu {num}.");
+            Console.WriteLine($"A soma foi {soma} → {(soma % 2 == 0 ? "PAR" : "IMPAR")}");
+
+            if ((soma % 2 == 0 && escolhaUsuario == "PAR") ||
+                (soma % 2 != 0 && escolhaUsuario == "IMPAR"))
+            {
+                Console.WriteLine("Você venceu, parabens!");
+            }
+            else
+            {
+                Console.WriteLine("O computador venceu, mais sorte na proxima");
+            }
+        }
+        else
+        {
+            Console.WriteLine("OK. Quando quiser jogar, só abrir o Visual");
+        }
+    }
---
+++ Autoria01-GuilhermeFerrari/Autoria01-GuilhermeFerrari/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string resposta;
+int nota;
+Console.WriteLine("Você acabou o jogo, e esse é um questionario feito para melhor o desempenho do jogo por meio de opinião de usuarios\n");
+Console.WriteLine("Gostaria de fazer o questionario? (s/n)");
+resposta = Console.ReadLine();
+if (resposta == "s")
+{
+    Console.WriteLine("Ok, vamos começar\n");
+    Console.WriteLine("Você gostou do jogo? (s/n)");
+    resposta = Console.ReadLine();
+    if (resposta == "s")
+    {
+        Console.WriteLine("\nOtimo! de 0 a 10 qual sua nota para o jogo?");
+        Console.WriteLine("Sendo 0 = Vast nas ultimas semanas e 10 = ponto extra pro Ferrari!");
+        nota = int.Parse(Console.ReadLine());
+        if (nota >= 7)
+        {
+            Console.WriteLine("Que bom! Agradeçemos sua aprovação, acredito que seja o suficiente para um extra!!!");
+        }
+        else
+        {
+            Console.WriteLine("porque?");
+            resposta = Console.ReadLine();
+            Console.WriteLine("Respeito seu feedback, mesmo você estando errado");
+        }
+    }
+    else
+    {
+        Console.WriteLine("porque?");
+        resposta = Console.ReadLine();
+        Console.WriteLine("Respeito sua opinião, mesmo você estando errado");
+    }
+}
+else
+{
+    Console.WriteLine("ok");
+}
